88f2b66969ec36c06ab59fcc3264dd3814f21a5f
Shalvin <shalvinshaji283@gmail.com>
2025-02-27 13:54:12 +0530

    Initial commit - Basic app setup completed

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "success" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Athlo admin API is running at port ${process.env.PORT}`);
+});
